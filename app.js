e9c363a980e4f519489db0d8b7bd5d17d256be47
Vishal Rajput <rajputvisha.vr@gmail.com>
2019-01-09 14:43:56 +0530

    updated the look and feel

--- app.js
+++
@@ -1,5 +0,0 @@
-//import fontawesome from '@fortawesome/fontawesome-free'
-//import regular from '@fortawesome/fontawesome-free/css/all.min.css'
-
-require("./utility");
-console.log("hello ");
--- utility.es6
+++
@@ -1,4 +0,0 @@
-let login = (username, password) => {
-    console.log("you tried logging");
-}
-console.log("utility loaded es6");
